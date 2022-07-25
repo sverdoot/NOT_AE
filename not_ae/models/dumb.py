0c66d5257b635dcd5c9324e4fa5115cec40edfcc
sverdoot <lagutin.em@phystech.edu>
2022-07-25 16:26:04 +0300

    fixed train loop

--- not_ae/models/dumb.py
+++ not_ae/models/dumb.py
@@ -8,6 +8,7 @@ from not_ae.utils.general import REGISTRY
 class Dumb_D(nn.Module):
     def __init__(self):
         super().__init__()
+        self.dumb_param = nn.Parameter(torch.empty(0))
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         return torch.zeros(x.shape[0], device=x.device)
